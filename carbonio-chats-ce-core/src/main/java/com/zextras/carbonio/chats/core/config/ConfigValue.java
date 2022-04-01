4d37992171b66c8911ca8a4bbc458eec5efe208d
Alberto Pontini <alberto.pontini@zextras.com>
2022-04-01 15:04:07 +0200

    [CHATS-90] introduced consul config names

--- carbonio-chats-ce-core/src/main/java/com/zextras/carbonio/chats/core/config/ConfigValue.java
+++ carbonio-chats-ce-core/src/main/java/com/zextras/carbonio/chats/core/config/ConfigValue.java
@@ -2,23 +2,26 @@ package com.zextras.carbonio.chats.core.config;
 
 public enum ConfigValue {
   ENV("chats.env", "CHATS_ENV", "TODO"),
-  JDBC_DRIVER("database.jdbc.driver", "DATABASE_JDBC_DRIVER", "TODO"),
-  DATABASE_JDBC_URL("database.jdbc.url", "DATABASE_JDBC_URL", "TODO"),
-  DATABASE_USERNAME("database.username", "DATABASE_USERNAME", "TODO"),
-  DATABASE_PASSWORD("database.password", "DATABASE_PASSWORD", "TODO"),
-  HIKARI_IDLE_TIMEOUT("datasource.hikari.idleTimeout", "HIKARI_IDLE_TIMEOUT", "TODO"),
-  HIKARI_MIN_POOL_SIZE("datasource.hikari.minPoolSize", "HIKARI_MIN_POOL_SIZE", "TODO"),
-  HIKARI_MAX_POOL_SIZE("datasource.hikari.maxPoolSize", "HIKARI_MAX_POOL_SIZE", "TODO"),
-  HIKARI_LEAK_DETECTION_THRESHOLD("datasource.hikari.leakDetectionThreshold", "HIKARI_LEAK_DETECTION_THRESHOLD",
+  JDBC_DRIVER("database.jdbc.driver", "DATABASE_JDBC_DRIVER", "jdbc-driver"),
+  DATABASE_JDBC_URL("database.jdbc.url", "DATABASE_JDBC_URL", "jdbc-url"),
+  DATABASE_USERNAME("database.username", "DATABASE_USERNAME", "db-username"),
+  DATABASE_PASSWORD("database.password", "DATABASE_PASSWORD", "db-password"),
+  HIKARI_IDLE_TIMEOUT("datasource.hikari.idleTimeout", "HIKARI_IDLE_TIMEOUT", "hikari-idle-timeout"),
+  HIKARI_MIN_POOL_SIZE("datasource.hikari.minPoolSize", "HIKARI_MIN_POOL_SIZE", "hikari-min-pool-size"),
+  HIKARI_MAX_POOL_SIZE("datasource.hikari.maxPoolSize", "HIKARI_MAX_POOL_SIZE", "hikari-max-pool-size"),
+  HIKARI_LEAK_DETECTION_THRESHOLD("datasource.hikari.leakDetectionThreshold", "hikari-leak-detection-threshold",
     "TODO"),
-  XMPP_SERVER_HOST("xmpp.host", "XMPP_HOST", "TODO"),
-  XMPP_SERVER_HTTP_PORT("xmpp.http.port", "XMPP_HTTP_PORT", "TODO"),
-  STORAGES_HOST("carbonio.storages.host", "STORAGES_HOST", "TODO"),
-  STORAGES_PORT("carbonio.storages.port", "STORAGES_PORT", "TODO"),
-  USER_MANAGEMENT_HOST("carbonio.usermanagement.host", "USER_MANAGEMENT_HOST", "TODO"),
-  USER_MANAGEMENT_PORT("carbonio.usermanagement.port", "USER_MANAGEMENT_PORT", "TODO"),
-  PREVIEWER_HOST("carbonio.preview.host", "PREVIEWER_HOST", "TODO"),
-  PREVIEWER_PORT("carbonio.preview.port", "PREVIEWER_PORT", "TODO");
+  XMPP_SERVER_HOST("xmpp.host", "XMPP_HOST", "xmpp-host"),
+  XMPP_SERVER_HTTP_PORT("xmpp.http.port", "XMPP_HTTP_PORT", "xmpp-http-port"),
+  STORAGES_HOST("carbonio.storages.host", "STORAGES_HOST", "storages-host"),
+  STORAGES_PORT("carbonio.storages.port", "STORAGES_PORT", "storages-port"),
+  USER_MANAGEMENT_HOST("carbonio.usermanagement.host", "USER_MANAGEMENT_HOST", "user-management-host"),
+  USER_MANAGEMENT_PORT("carbonio.usermanagement.port", "USER_MANAGEMENT_PORT", "user-management-port"),
+  PREVIEWER_HOST("carbonio.preview.host", "PREVIEWER_HOST", "previewer-host"),
+  PREVIEWER_PORT("carbonio.preview.port", "PREVIEWER_PORT", "previewer-port"),
+  CONSUL_HOST("consul.agent.host", "CONSUL_HOST", "consul-host"),
+  CONSUL_PORT("consul.agent.port", "CONSUL_PORT", "consul-port"),
+  CONSUL_TOKEN("consul.agent.token", "CONSUL_TOKEN", "consul-token");
 
 
   private final String propertyName;
