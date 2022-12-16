2cd4d095e96d1abfb424b33bb62fe97e0b2c9582
Luca Gasparini <luca.gasparini@zextras.com>
2022-12-16 11:17:09 +0000

    feat: [CHATS-486] allow a session to open or to close the screen share stream
    Approved-by: Noman Ali Shaukat

---
+++ carbonio-chats-ce-core/src/main/java/com/zextras/carbonio/chats/core/data/event/MeetingParticipantScreenStreamClosed.java
@@ -0,0 +1,60 @@
+package com.zextras.carbonio.chats.core.data.event;
+
+import java.util.Objects;
+import java.util.UUID;
+import org.jetbrains.annotations.Nullable;
+
+public class MeetingParticipantScreenStreamClosed extends DomainEvent {
+
+  private static final EventType EVENT_TYPE = EventType.MEETING_PARTICIPANT_SCREEN_STREAM_CLOSED;
+
+  private UUID   meetingId;
+  private String sessionId;
+
+  public MeetingParticipantScreenStreamClosed(UUID from, @Nullable String sessionId) {
+    super(EVENT_TYPE, from, sessionId);
+  }
+
+  public static MeetingParticipantScreenStreamClosed create(UUID from, @Nullable String sessionId) {
+    return new MeetingParticipantScreenStreamClosed(from, sessionId);
+  }
+
+  public UUID getMeetingId() {
+    return meetingId;
+  }
+
+  public MeetingParticipantScreenStreamClosed meetingId(UUID meetingId) {
+    this.meetingId = meetingId;
+    return this;
+  }
+
+  @Override
+  public String getSessionId() {
+    return sessionId;
+  }
+
+  public MeetingParticipantScreenStreamClosed sessionId(String sessionId) {
+    this.sessionId = sessionId;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    MeetingParticipantScreenStreamClosed that = (MeetingParticipantScreenStreamClosed) o;
+    return Objects.equals(getMeetingId(), that.getMeetingId()) && Objects.equals(getSessionId(), that.getSessionId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), getMeetingId(), getSessionId());
+  }
+}
