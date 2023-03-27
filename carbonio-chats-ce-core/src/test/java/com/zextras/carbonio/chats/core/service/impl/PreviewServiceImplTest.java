5445e4794fdb1210e5f707f74ff24334aa83ac5b
Domenico Stragliotto <domenico.stragliotto@zextras.com>
2023-03-27 14:44:35 +0200

    feat: Add ut for PreviewService

---
+++ carbonio-chats-ce-core/src/test/java/com/zextras/carbonio/chats/core/service/impl/PreviewServiceImplTest.java
@@ -0,0 +1,309 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.chats.core.service.impl;
+
+import com.zextras.carbonio.chats.core.annotations.UnitTest;
+import com.zextras.carbonio.chats.core.config.AppConfig;
+import com.zextras.carbonio.chats.core.config.ConfigName;
+import com.zextras.carbonio.chats.core.data.entity.FileMetadata;
+import com.zextras.carbonio.chats.core.data.entity.FileMetadataBuilder;
+import com.zextras.carbonio.chats.core.data.entity.Room;
+import com.zextras.carbonio.chats.core.data.entity.Subscription;
+import com.zextras.carbonio.chats.core.data.entity.User;
+import com.zextras.carbonio.chats.core.data.event.UserPictureChangedEvent;
+import com.zextras.carbonio.chats.core.data.event.UserPictureDeletedEvent;
+import com.zextras.carbonio.chats.core.data.model.FileContentAndMetadata;
+import com.zextras.carbonio.chats.core.data.model.FileResponse;
+import com.zextras.carbonio.chats.core.data.model.UserProfile;
+import com.zextras.carbonio.chats.core.data.type.FileMetadataType;
+import com.zextras.carbonio.chats.core.exception.BadRequestException;
+import com.zextras.carbonio.chats.core.exception.ChatsHttpException;
+import com.zextras.carbonio.chats.core.exception.ForbiddenException;
+import com.zextras.carbonio.chats.core.exception.NotFoundException;
+import com.zextras.carbonio.chats.core.infrastructure.event.EventDispatcher;
+import com.zextras.carbonio.chats.core.infrastructure.profiling.ProfilingService;
+import com.zextras.carbonio.chats.core.infrastructure.storage.StoragesService;
+import com.zextras.carbonio.chats.core.repository.FileMetadataRepository;
+import com.zextras.carbonio.chats.core.repository.SubscriptionRepository;
+import com.zextras.carbonio.chats.core.repository.UserRepository;
+import com.zextras.carbonio.chats.core.service.PreviewService;
+import com.zextras.carbonio.chats.core.service.RoomService;
+import com.zextras.carbonio.chats.core.service.UserService;
+import com.zextras.carbonio.chats.core.utils.Utils;
+import com.zextras.carbonio.chats.core.web.security.UserPrincipal;
+import com.zextras.carbonio.chats.model.ImageQualityEnumDto;
+import com.zextras.carbonio.chats.model.ImageShapeEnumDto;
+import com.zextras.carbonio.chats.model.ImageTypeEnumDto;
+import com.zextras.carbonio.chats.model.RoomTypeDto;
+import com.zextras.carbonio.chats.model.UserDto;
+import com.zextras.carbonio.preview.PreviewClient;
+import com.zextras.carbonio.preview.queries.BlobResponse;
+import com.zextras.carbonio.preview.queries.Query;
+import com.zextras.carbonio.preview.queries.enums.Format;
+import com.zextras.carbonio.preview.queries.enums.Quality;
+import com.zextras.carbonio.preview.queries.enums.ServiceType;
+import com.zextras.carbonio.preview.queries.enums.Shape;
+import io.vavr.control.Option;
+import io.vavr.control.Try;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.HttpEntityWrapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import javax.ws.rs.core.Response.Status;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@UnitTest
+class PreviewServiceImplTest {
+
+  private final RoomService roomService;
+  private final FileMetadataRepository fileMetadataRepository;
+  private final PreviewClient previewClient;
+  private final PreviewService previewService;
+  private static Room room1;
+  private static UUID user1Id;
+  private static UUID user2Id;
+
+  public PreviewServiceImplTest() {
+    this.roomService = mock(RoomService.class);
+    this.fileMetadataRepository = mock(FileMetadataRepository.class);
+    this.previewClient = mock(PreviewClient.class);
+    this.previewService = new PreviewServiceImpl(
+      roomService,
+      fileMetadataRepository,
+      previewClient);
+  }
+
+  @BeforeAll
+  public static void setup(){
+    user1Id = UUID.randomUUID();
+    user2Id = UUID.randomUUID();
+    room1 = Room.create();
+    room1
+      .id(UUID.randomUUID().toString())
+      .type(RoomTypeDto.GROUP)
+      .name("room1")
+      .description("Room one")
+      .subscriptions(List.of(
+        Subscription.create(room1, user1Id.toString()).owner(true),
+        Subscription.create(room1, user2Id.toString()).owner(false))
+      );
+  }
+
+  @BeforeEach
+  public void init() {
+  }
+
+  @Test
+  @DisplayName("Returns the preview of an image")
+  public void getImagePreview() throws IOException {
+    UUID fileId = UUID.randomUUID();
+    UserPrincipal currentUser = UserPrincipal.create(user1Id);
+    FileMetadata expectedMetadata = FileMetadataBuilder.create().id(fileId.toString())
+      .name("snoopy.jpg").mimeType("image/jpeg").type(FileMetadataType.ATTACHMENT)
+      .userId(user1Id.toString()).roomId(room1.getId()).build();
+    when(fileMetadataRepository.getById(fileId.toString())).thenReturn(Optional.of(expectedMetadata));
+    when(roomService.getRoomEntityAndCheckUser(UUID.fromString(room1.getId()), currentUser, false)).thenReturn(room1);
+    Query parameters = new Query.QueryBuilder()
+      .setFileOwnerId(user1Id.toString())
+      .setServiceType(ServiceType.CHATS)
+      .setFileId(fileId.toString())
+      .setVersion(1)
+      .setPreviewArea("100x100")
+      .setQuality(Quality.HIGH)
+      .setOutputFormat(Format.JPEG)
+      .setCrop(false).build();
+    BlobResponse mockBlobResponse = mock(BlobResponse.class);
+    when(mockBlobResponse.getContent()).thenReturn(new ByteArrayInputStream("image".getBytes()));
+    when(mockBlobResponse.getLength()).thenReturn(5L);
+    when(mockBlobResponse.getMimeType()).thenReturn("image/jpeg");
+    ArgumentCaptor<Query> parametersCapture = ArgumentCaptor.forClass(Query.class);
+    when(previewClient.getPreviewOfImage(any(Query.class))).thenReturn(Try.success(mockBlobResponse));
+    FileResponse previewImageResponse = previewService.getImage(currentUser,
+      fileId,
+      "100x100",
+      Option.of(ImageQualityEnumDto.HIGH),
+      Option.of(ImageTypeEnumDto.JPEG),
+      Option.of(false));
+
+    verify(previewClient,times(1)).getPreviewOfImage(parametersCapture.capture());
+    assertEquals(parametersCapture.getValue().toString(), parameters.toString());
+    assertEquals(new String(Files.readAllBytes(previewImageResponse.getContent().toPath())),
+      "image");
+    assertEquals(previewImageResponse.getLength(),5);
+    assertEquals(previewImageResponse.getMimeType(),"image/jpeg");
+  }
+
+  @Test
+  @DisplayName("Returns the thumbnail of an image")
+  public void getImageThumbnail() throws IOException {
+    UUID fileId = UUID.randomUUID();
+    UserPrincipal currentUser = UserPrincipal.create(user1Id);
+    FileMetadata expectedMetadata = FileMetadataBuilder.create().id(fileId.toString())
+      .name("snoopy.jpg").mimeType("image/jpeg").type(FileMetadataType.ATTACHMENT)
+      .userId(user1Id.toString()).roomId(room1.getId()).build();
+    when(fileMetadataRepository.getById(fileId.toString())).thenReturn(Optional.of(expectedMetadata));
+    when(roomService.getRoomEntityAndCheckUser(UUID.fromString(room1.getId()), currentUser, false)).thenReturn(room1);
+    Query parameters = new Query.QueryBuilder()
+      .setFileOwnerId(user1Id.toString())
+      .setServiceType(ServiceType.CHATS)
+      .setFileId(fileId.toString())
+      .setVersion(1)
+      .setPreviewArea("100x100")
+      .setQuality(Quality.HIGH)
+      .setOutputFormat(Format.JPEG)
+      .setShape(Shape.RECTANGULAR)
+      .build();
+    BlobResponse mockBlobResponse = mock(BlobResponse.class);
+    when(mockBlobResponse.getContent()).thenReturn(new ByteArrayInputStream("image".getBytes()));
+    when(mockBlobResponse.getLength()).thenReturn(5L);
+    when(mockBlobResponse.getMimeType()).thenReturn("image/jpeg");
+    ArgumentCaptor<Query> parametersCapture = ArgumentCaptor.forClass(Query.class);
+    when(previewClient.getPreviewOfImage(any(Query.class))).thenReturn(Try.success(mockBlobResponse));
+    FileResponse previewImageResponse = previewService.getImageThumbnail(currentUser,
+      fileId,
+      "100x100",
+      Option.of(ImageQualityEnumDto.HIGH),
+      Option.of(ImageTypeEnumDto.JPEG),
+      Option.of(ImageShapeEnumDto.RECTANGULAR));
+
+    verify(previewClient,times(1)).getPreviewOfImage(parametersCapture.capture());
+    assertEquals(parametersCapture.getValue().toString(), parameters.toString());
+    assertEquals(new String(Files.readAllBytes(previewImageResponse.getContent().toPath())),
+      "image");
+    assertEquals(previewImageResponse.getLength(),5);
+    assertEquals(previewImageResponse.getMimeType(),"image/jpeg");
+  }
+
+  @Test
+  @DisplayName("Returns the preview of a pdf")
+  public void getPDFPreview() throws IOException {
+    UUID fileId = UUID.randomUUID();
+    UserPrincipal currentUser = UserPrincipal.create(user1Id);
+    FileMetadata expectedMetadata = FileMetadataBuilder.create().id(fileId.toString())
+      .name("test.pdf").mimeType("application/pdf").type(FileMetadataType.ATTACHMENT)
+      .userId(user1Id.toString()).roomId(room1.getId()).build();
+    when(fileMetadataRepository.getById(fileId.toString())).thenReturn(Optional.of(expectedMetadata));
+    when(roomService.getRoomEntityAndCheckUser(UUID.fromString(room1.getId()), currentUser, false)).thenReturn(room1);
+    Query parameters = new Query.QueryBuilder()
+      .setFileOwnerId(user1Id.toString())
+      .setServiceType(ServiceType.CHATS)
+      .setFileId(fileId.toString())
+      .setVersion(1)
+      .setFirstPage(1)
+      .setLastPage(0)
+      .build();
+    BlobResponse mockBlobResponse = mock(BlobResponse.class);
+    when(mockBlobResponse.getContent()).thenReturn(new ByteArrayInputStream("pdf".getBytes()));
+    when(mockBlobResponse.getLength()).thenReturn(3L);
+    when(mockBlobResponse.getMimeType()).thenReturn("application/pdf");
+    ArgumentCaptor<Query> parametersCapture = ArgumentCaptor.forClass(Query.class);
+    when(previewClient.getPreviewOfImage(any(Query.class))).thenReturn(Try.success(mockBlobResponse));
+    FileResponse previewImageResponse = previewService.getPDF(currentUser,
+      fileId,
+      1,
+      0);
+
+    verify(previewClient,times(1)).getPreviewOfImage(parametersCapture.capture());
+    assertEquals(parametersCapture.getValue().toString(), parameters.toString());
+    assertEquals(new String(Files.readAllBytes(previewImageResponse.getContent().toPath())),
+      "pdf");
+    assertEquals(previewImageResponse.getLength(),3);
+    assertEquals(previewImageResponse.getMimeType(),"application/pdf");
+  }
+
+  @Test
+  @DisplayName("Returns the thumbnail of a pdf")
+  public void getPDFThumbnail() throws IOException {
+    UUID fileId = UUID.randomUUID();
+    UserPrincipal currentUser = UserPrincipal.create(user1Id);
+    FileMetadata expectedMetadata = FileMetadataBuilder.create().id(fileId.toString())
+      .name("test.pdf").mimeType("application/pdf").type(FileMetadataType.ATTACHMENT)
+      .userId(user1Id.toString()).roomId(room1.getId()).build();
+    when(fileMetadataRepository.getById(fileId.toString())).thenReturn(Optional.of(expectedMetadata));
+    when(roomService.getRoomEntityAndCheckUser(UUID.fromString(room1.getId()), currentUser, false)).thenReturn(room1);
+    Query parameters = new Query.QueryBuilder()
+      .setFileOwnerId(user1Id.toString())
+      .setServiceType(ServiceType.CHATS)
+      .setFileId(fileId.toString())
+      .setVersion(1)
+      .setPreviewArea("100x100")
+      .setQuality(Quality.HIGH)
+      .setOutputFormat(Format.JPEG)
+      .setShape(Shape.RECTANGULAR)
+      .build();
+    BlobResponse mockBlobResponse = mock(BlobResponse.class);
+    when(mockBlobResponse.getContent()).thenReturn(new ByteArrayInputStream("pdf".getBytes()));
+    when(mockBlobResponse.getLength()).thenReturn(3L);
+    when(mockBlobResponse.getMimeType()).thenReturn("application/pdf");
+    ArgumentCaptor<Query> parametersCapture = ArgumentCaptor.forClass(Query.class);
+    when(previewClient.getPreviewOfImage(any(Query.class))).thenReturn(Try.success(mockBlobResponse));
+    FileResponse previewImageResponse = previewService.getImageThumbnail(currentUser,
+      fileId,
+      "100x100",
+      Option.of(ImageQualityEnumDto.HIGH),
+      Option.of(ImageTypeEnumDto.JPEG),
+      Option.of(ImageShapeEnumDto.RECTANGULAR));
+
+    verify(previewClient,times(1)).getPreviewOfImage(parametersCapture.capture());
+    assertEquals(parametersCapture.getValue().toString(), parameters.toString());
+    assertEquals(new String(Files.readAllBytes(previewImageResponse.getContent().toPath())),
+      "pdf");
+    assertEquals(previewImageResponse.getLength(),3);
+    assertEquals(previewImageResponse.getMimeType(),"application/pdf");
+  }
+
+  @Test
+  @DisplayName("Returns error if user is not in the room")
+  public void getImagePreviewNotAuthorized() throws IOException {
+    UUID fileId = UUID.randomUUID();
+    UserPrincipal currentUser = UserPrincipal.create(user1Id);
+    FileMetadata expectedMetadata = FileMetadataBuilder.create().id(fileId.toString())
+      .name("snoopy.jpg").mimeType("image/jpeg").type(FileMetadataType.ATTACHMENT)
+      .userId(user1Id.toString()).roomId(room1.getId()).build();
+    when(fileMetadataRepository.getById(fileId.toString())).thenReturn(Optional.of(expectedMetadata));
+    when(roomService.getRoomEntityAndCheckUser(UUID.fromString(room1.getId()), currentUser, false))
+      .thenThrow(new ForbiddenException());
+
+    assertThrows(ForbiddenException.class, () -> previewService.getImage(currentUser,
+      fileId,
+      "100x100",
+      Option.of(ImageQualityEnumDto.HIGH),
+      Option.of(ImageTypeEnumDto.JPEG),
+      Option.of(false)));
+  }
+
+}
