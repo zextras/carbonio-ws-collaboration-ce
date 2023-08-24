6c7712b5f156b6f96232ec3ed0272f1dafd4d11c
Noman <noman.alishaukat@zextras.com>
2023-08-24 11:06:28 +0200

    chore: fix chats-api-yaml path and add meeting-api.yaml in catalog-info

--- carbonio-ws-collaboration-core/src/main/resources/migration/V1_0_1__add_workspace_fields.sql
+++ carbonio-ws-collaboration-core/src/main/resources/migration/V1_0_1__add_workspace_fields.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+
 ALTER TABLE CHATS.ROOM
     ADD COLUMN PARENT_ID          VARCHAR(64),
     ADD COLUMN RANK               INTEGER,
