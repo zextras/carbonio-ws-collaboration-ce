fcd7d073e62043a12a747484590241252b13e152
Noman <99014456+nomanAli95@users.noreply.github.com>
2023-08-24 09:49:34 +0200

    chore: WSC-508 implement IT tests for video server (#63)
    * update test environments dependencies
    * comment videoservereventlistener in Boot class
    
    ---------
    
    Co-authored-by: Luca Gasparini <luca.gasparini@zextras.com>

---
+++ carbonio-ws-collaboration-it/src/test/java/com/zextras/carbonio/chats/it/tools/VideoServerMockServer.java
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.chats.it.tools;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import com.zextras.carbonio.chats.core.logging.ChatsLogger;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Nullable;
+import org.junit.jupiter.api.extension.ExtensionContext.Store.CloseableResource;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.ClearType;
+import org.mockserver.model.Header;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.JsonBody;
+import org.mockserver.verify.VerificationTimes;
+
+public class VideoServerMockServer extends ClientAndServer implements CloseableResource {
+
+  public VideoServerMockServer(Integer... ports) {
+    super(ports);
+  }
+
+  public VideoServerMockServer(String remoteHost, Integer remotePort, Integer... ports) {
+    super(remoteHost, remotePort, ports);
+  }
+
+  public void verify(String method, String path, @Nullable Map<String, String> queryParameters, int iterationsNumber) {
+    HttpRequest request = request()
+      .withMethod(method)
+      .withPath(path);
+    Optional.ofNullable(queryParameters).ifPresent(parameters ->
+      parameters.forEach(request::withQueryStringParameter
+      ));
+    verify(request, VerificationTimes.exactly(iterationsNumber));
+    clear(request, ClearType.LOG);
+  }
+
+  public void setIsAliveResponse(boolean success) {
+    HttpRequest request = request().withMethod("GET").withPath("/health/ready/");
+    clear(request);
+    when(request)
+      .respond(
+        response()
+          .withStatusCode(success ? 200 : 500)
+      );
+  }
+
+  @Override
+  public void close() {
+    ChatsLogger.debug("Stopping Video Server mock...");
+    super.close();
+  }
+
+  public HttpRequest getRequest(String method, String path, String body) {
+    return request()
+      .withMethod(method)
+      .withPath(path)
+      .withHeader(Header.header("content-type", "application/json"))
+      .withBody(
+        JsonBody.json(body));
+  }
+
+  public void mockRequestedResponse(
+    String method, String path, String requestBody, String responseBody, boolean success
+  ) {
+    HttpRequest request = getRequest(method, path, requestBody);
+    clear(request);
+    when(request).respond(response()
+      .withStatusCode(success ? 200 : 500)
+      .withBody(JsonBody.json(responseBody)));
+  }
+}
--- carbonio-ws-collaboration-it/src/test/java/com/zextras/carbonio/chats/it/utils/MeetingTestUtils.java
+++ carbonio-ws-collaboration-it/src/test/java/com/zextras/carbonio/chats/it/utils/MeetingTestUtils.java
@@ -6,43 +6,53 @@ package com.zextras.carbonio.chats.it.utils;
 
 import com.zextras.carbonio.chats.core.data.entity.Meeting;
 import com.zextras.carbonio.chats.core.data.entity.Participant;
+import com.zextras.carbonio.chats.core.data.entity.VideoServerMeeting;
+import com.zextras.carbonio.chats.core.data.entity.VideoServerSession;
 import com.zextras.carbonio.chats.core.data.type.MeetingType;
 import com.zextras.carbonio.chats.core.repository.MeetingRepository;
 import com.zextras.carbonio.chats.core.repository.ParticipantRepository;
+import com.zextras.carbonio.chats.core.repository.VideoServerMeetingRepository;
+import com.zextras.carbonio.chats.core.repository.VideoServerSessionRepository;
 import com.zextras.carbonio.chats.it.entity.ParticipantBuilder;
-
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class MeetingTestUtils {
 
-  private final MeetingRepository     meetingRepository;
-  private final ParticipantRepository participantRepository;
+  private final MeetingRepository            meetingRepository;
+  private final ParticipantRepository        participantRepository;
+  private final VideoServerMeetingRepository videoServerMeetingRepository;
+  private final VideoServerSessionRepository videoServerSessionRepository;
 
   @Inject
   public MeetingTestUtils(
     MeetingRepository meetingRepository,
-    ParticipantRepository participantRepository
+    ParticipantRepository participantRepository,
+    VideoServerMeetingRepository videoServerMeetingRepository,
+    VideoServerSessionRepository videoServerSessionRepository
   ) {
     this.meetingRepository = meetingRepository;
     this.participantRepository = participantRepository;
+    this.videoServerMeetingRepository = videoServerMeetingRepository;
+    this.videoServerSessionRepository = videoServerSessionRepository;
   }
 
   public UUID generateAndSaveMeeting(UUID roomId, List<ParticipantBuilder> participantBuilders) {
     return generateAndSaveMeeting(roomId, participantBuilders, false, null);
   }
 
-  public UUID generateAndSaveMeeting(UUID roomId,
-                                     List<ParticipantBuilder> participantBuilders,
-                                     Boolean active,
-                                     OffsetDateTime expiration){
+  public UUID generateAndSaveMeeting(
+    UUID roomId,
+    List<ParticipantBuilder> participantBuilders,
+    Boolean active,
+    OffsetDateTime expiration
+  ) {
     Meeting meeting = meetingRepository.insert("Test Meeting for " + roomId.toString(),
       MeetingType.PERMANENT,
       roomId,
@@ -60,8 +70,27 @@ public class MeetingTestUtils {
   }
 
   public Optional<Participant> getParticipant(UUID meetingId, String sessionId) {
-    return
-      participantRepository.getById(meetingId.toString(), sessionId);
+    return participantRepository.getById(meetingId.toString(), sessionId);
+  }
+
+  public VideoServerMeeting insertVideoServerMeeting(
+    String meetingId, String connectionId, String audioHandleId, String videoHandleId, String audioRoomId,
+    String videoRoomId
+  ) {
+    return videoServerMeetingRepository.insert(meetingId, connectionId, audioHandleId,
+      videoHandleId, audioRoomId, videoRoomId);
+  }
+
+  public VideoServerSession insertVideoServerSession(
+    VideoServerMeeting videoServerMeeting, String sessionId, String connectionId,
+    String videoOutHandleId, String screenHandleId
+  ) {
+    return videoServerSessionRepository.insert(videoServerMeeting, sessionId, connectionId,
+      videoOutHandleId, screenHandleId);
+  }
+
+  public VideoServerSession updateVideoServerSession(VideoServerSession videoServerSession) {
+    return videoServerSessionRepository.update(videoServerSession);
   }
 
 }
