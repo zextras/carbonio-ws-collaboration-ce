a40bc4f66f2cbec0a21cf0ca80a4961ad7c34859
Noman <99014456+nomanAli95@users.noreply.github.com>
2023-11-22 11:35:56 +0100

    feat: WSC-1146 Add video-call configs (#76) (#32)
    * add can_video_call
    * add can_video_call_record
    * add can_use_virtual_background
    * update pre-commit file
    * bump package version to 0.1.9

--- carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/config/ChatsConstant.java
+++ carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/config/ChatsConstant.java
@@ -6,20 +6,23 @@ package com.zextras.carbonio.chats.core.config;
 
 public class ChatsConstant {
 
-  public static final String SERVER_HOST                 = "127.78.0.4";
-  public static final int    SERVER_PORT                 = 10000;
-  public static final String LOGGER_CONFIG_PATH          = "/etc/carbonio/ws-collaboration/logback.xml";
+  public static final String SERVER_HOST = "127.78.0.4";
+  public static final int SERVER_PORT = 10000;
+  public static final String LOGGER_CONFIG_PATH = "/etc/carbonio/ws-collaboration/logback.xml";
   public static final String MONGOOSEIM_GRAPHQL_ENDPOINT = "api/graphql";
-  public static final String IS_UNICODE_FORMAT_REGEX     = "[\\\\[uU][a-fA-F0-9]{4}]*";
+  public static final String IS_UNICODE_FORMAT_REGEX = "[\\\\[uU][a-fA-F0-9]{4}]*";
 
   public static class CONFIGURATIONS_DEFAULT_VALUES {
 
-    public static final boolean CAN_SEE_MESSAGE_READS                = true;
-    public static final boolean CAN_SEE_USERS_PRESENCE               = true;
-    public static final int     MAX_USER_IMAGE_SIZE_IN_KB            = 512;
-    public static final int     MAX_ROOM_IMAGE_SIZE_IN_KB            = 512;
-    public static final int     EDIT_MESSAGE_TIME_LIMIT_IN_MINUTES   = 10;
-    public static final int     DELETE_MESSAGE_TIME_LIMIT_IN_MINUTES = 10;
-    public static final int     MAX_GROUP_MEMBERS                    = 128;
+    public static final boolean CAN_VIDEO_CALL = true;
+    public static final boolean CAN_VIDEO_CALL_RECORD = false;
+    public static final boolean CAN_USE_VIRTUAL_BACKGROUND = false;
+    public static final boolean CAN_SEE_MESSAGE_READS = true;
+    public static final boolean CAN_SEE_USERS_PRESENCE = true;
+    public static final int MAX_USER_IMAGE_SIZE_IN_KB = 512;
+    public static final int MAX_ROOM_IMAGE_SIZE_IN_KB = 512;
+    public static final int EDIT_MESSAGE_TIME_LIMIT_IN_MINUTES = 10;
+    public static final int DELETE_MESSAGE_TIME_LIMIT_IN_MINUTES = 10;
+    public static final int MAX_GROUP_MEMBERS = 128;
   }
 }
