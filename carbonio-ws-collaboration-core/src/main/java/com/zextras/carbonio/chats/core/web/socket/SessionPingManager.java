91edf95e7382d33dd82c57be9ee8c282ba6bf417
Noman <99014456+nomanAli95@users.noreply.github.com>
2025-09-01 17:42:06 +0200

    fix: leak memory cancelling scheduled ping task on session close (#125)
    * chore: replace cache with concurrent hash map and remove 60 seconds timeout
    * chore: increment ping interval to 60 seconds
    * chore: set scheduler thread as deamon and remove shutdown method
    
    ref: CO-2932

--- carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/web/socket/SessionPingManager.java
+++ carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/web/socket/SessionPingManager.java
@@ -4,72 +4,52 @@
 
 package com.zextras.carbonio.chats.core.web.socket;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.RemovalCause;
-import com.github.benmanes.caffeine.cache.Scheduler;
 import com.zextras.carbonio.chats.core.logging.ChatsLogger;
-import jakarta.websocket.PongMessage;
 import jakarta.websocket.Session;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SessionPingManager {
 
-  private static final int PING_INTERVAL = 15;
-  private static final int THREAD_POOL_SIZE = 100;
+  private static final int PING_INTERVAL = 30;
+  private static final int ARBITRARY_BYTE = 0x01;
+  private static final ByteBuffer PING_PAYLOAD = ByteBuffer.wrap(new byte[] {ARBITRARY_BYTE});
+
+  private static final AtomicInteger THREAD_NUMBER = new AtomicInteger(1);
+  private static final int THREAD_POOL_SIZE =
+      Math.max(4, Runtime.getRuntime().availableProcessors());
+
+  private static final ConcurrentHashMap<Session, ScheduledFuture<?>> ACTIVE_SESSIONS =
+      new ConcurrentHashMap<>();
 
   private static final ScheduledExecutorService scheduler =
-      Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
-
-  private static final Cache<Session, Long> sessionCache =
-      Caffeine.newBuilder()
-          .expireAfterWrite(60, TimeUnit.SECONDS)
-          .scheduler(Scheduler.systemScheduler())
-          .removalListener(
-              (session, timestamp, cause) -> {
-                if ((RemovalCause.EXPIRED.equals(cause) || RemovalCause.EXPLICIT.equals(cause))
-                    && session != null) {
-                  Session s = (Session) session;
-                  closeSession(s);
-                }
-              })
-          .build();
-
-  private SessionPingManager() {
-    Runtime.getRuntime()
-        .addShutdownHook(
-            new Thread(SessionPingManager::shutdown, "Session ping manager shutdown hook"));
-  }
+      Executors.newScheduledThreadPool(THREAD_POOL_SIZE, r -> createDaemonThread(r));
 
   public static void add(Session session) {
-    session.addMessageHandler(
-        PongMessage.class,
-        pongMessage -> {
-          sessionCache.put(session, System.currentTimeMillis());
-        });
-
-    scheduler.scheduleAtFixedRate(
-        () -> {
-          if (session.isOpen()) {
-            try {
-              session
-                  .getAsyncRemote()
-                  .sendPing(ByteBuffer.wrap("ping".getBytes(StandardCharsets.UTF_8)));
-            } catch (IOException e) {
-              ChatsLogger.warn("Error sending ping to websocket session " + session.getId());
-              sessionCache.invalidate(session);
-            }
-          }
-        },
-        0,
-        PING_INTERVAL,
-        TimeUnit.SECONDS);
-    sessionCache.put(session, System.currentTimeMillis());
+    ScheduledFuture<?> future =
+        scheduler.scheduleAtFixedRate(
+            () -> {
+              if (session.isOpen()) {
+                try {
+                  session.getAsyncRemote().sendPing(PING_PAYLOAD);
+                } catch (IOException e) {
+                  ChatsLogger.warn("Error sending ping to websocket session " + session.getId());
+                  remove(session);
+                }
+              } else {
+                remove(session);
+              }
+            },
+            0,
+            PING_INTERVAL,
+            TimeUnit.SECONDS);
+    ACTIVE_SESSIONS.put(session, future);
   }
 
   private static void closeSession(Session session) {
@@ -83,11 +63,17 @@ public class SessionPingManager {
   }
 
   public static void remove(Session session) {
-    sessionCache.invalidate(session);
+    ScheduledFuture<?> future = ACTIVE_SESSIONS.remove(session);
+    if (future != null) {
+      future.cancel(true);
+    }
+    closeSession(session);
   }
 
-  public static void shutdown() {
-    sessionCache.invalidateAll();
-    scheduler.shutdownNow();
+  private static Thread createDaemonThread(Runnable r) {
+    Thread thread =
+        new Thread(r, "SessionPingManager-Scheduler-" + THREAD_NUMBER.getAndIncrement());
+    thread.setDaemon(true);
+    return thread;
   }
 }
