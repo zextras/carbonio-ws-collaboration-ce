56f89886370537ba07cee6285e72f60ef6f803a2
Matteo Galvagni <50049534+galvagnimatteo@users.noreply.github.com>
2025-11-10 09:52:51 +0100

    fix: filter out non active/non feature-enabled users (#173)
    Refs: CO-2706

--- carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/web/security/EventsWebSocketAuthenticationFilter.java
+++ carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/web/security/EventsWebSocketAuthenticationFilter.java
@@ -6,6 +6,9 @@ package com.zextras.carbonio.chats.core.web.security;
 
 import com.zextras.carbonio.chats.core.infrastructure.authentication.AuthenticationService;
 import com.zextras.carbonio.chats.core.logging.ChatsLogger;
+import com.zextras.carbonio.usermanagement.entities.UserMyself;
+import com.zextras.carbonio.usermanagement.enumerations.UserStatus;
+import com.zextras.carbonio.usermanagement.enumerations.UserType;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -36,25 +39,50 @@ public class EventsWebSocketAuthenticationFilter implements Filter {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
     Optional<String> authToken =
         Arrays.stream(Optional.ofNullable(httpRequest.getCookies()).orElse(new Cookie[] {}))
             .filter(cookie -> AuthenticationMethod.ZM_AUTH_TOKEN.name().equals(cookie.getName()))
             .findAny()
             .map(Cookie::getValue);
+
     if (authToken.isEmpty()) {
       ChatsLogger.warn("Websocket authentication failed with an empty token");
-      HttpServletResponse httpServletResponse = (HttpServletResponse) response;
       httpServletResponse.setStatus(Status.UNAUTHORIZED.getStatusCode());
       return;
     }
-    Optional<String> userId = authenticationService.validateCredentials(authToken.get());
-    if (userId.isEmpty()) {
+
+    Optional<UserMyself> userMyselfOpt = authenticationService.getUserMyself(authToken.get());
+
+    if (userMyselfOpt.isEmpty()) {
       ChatsLogger.warn("Websocket authentication failed for token " + authToken.get());
-      HttpServletResponse httpServletResponse = (HttpServletResponse) response;
       httpServletResponse.setStatus(Status.UNAUTHORIZED.getStatusCode());
       return;
     }
-    httpRequest.getSession().setAttribute("userId", userId.get());
+
+    UserMyself userMyself = userMyselfOpt.get();
+
+    // Check if user status is ACTIVE
+    if (!userMyself.getStatus().equals(UserStatus.ACTIVE)) {
+      ChatsLogger.warn("Websocket authentication failed: user is not active");
+      httpServletResponse.setStatus(Status.UNAUTHORIZED.getStatusCode());
+      return;
+    }
+
+    // Check if carbonioFeatureWscEnabled is TRUE
+    // TODO we only do this check for internal users, since guests have all carbonioFeatureFlags set to false;
+    // this can and should probably change in the future
+    String wscEnabled = userMyself.getCarbonioAttributes()
+        .getOrDefault("carbonioFeatureWscEnabled", "FALSE");
+    if (wscEnabled.equals("FALSE") && userMyself.getType().equals(UserType.INTERNAL)) {
+      ChatsLogger.warn("Websocket authentication failed: WSC feature not enabled for user");
+      httpServletResponse.setStatus(Status.UNAUTHORIZED.getStatusCode());
+      return;
+    }
+
+    String userId = userMyself.getId().getUserId();
+    httpRequest.getSession().setAttribute("userId", userId);
     chain.doFilter(request, response);
   }
 }
