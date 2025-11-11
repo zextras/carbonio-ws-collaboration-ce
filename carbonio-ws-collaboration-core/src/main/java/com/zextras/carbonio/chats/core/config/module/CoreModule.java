0690710a7d43f8dd08166bdc833d5fdc6a81d2a1
Matteo Baglini <matteo.baglini@gmail.com>
2025-11-11 18:52:06 +0100

    chore: disable ssl connection and specify application name

--- carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/config/module/CoreModule.java
+++ carbonio-ws-collaboration-core/src/main/java/com/zextras/carbonio/chats/core/config/module/CoreModule.java
@@ -129,6 +129,7 @@ import java.io.IOException;
 import java.time.Clock;
 import java.time.ZoneId;
 import java.util.Base64;
+import java.util.Properties;
 import java.util.concurrent.TimeoutException;
 import org.flywaydb.core.Flyway;
 
@@ -299,6 +300,12 @@ public class CoreModule extends AbstractModule {
         appConfig.get(Integer.class, ConfigName.HIKARI_LEAK_DETECTION_THRESHOLD).orElse(5000));
     config.setMaxLifetime(
         appConfig.get(Integer.class, ConfigName.HIKARI_MAX_LIFETIME).orElse(600000));
+
+    Properties properties = new Properties();
+    properties.setProperty("sslmode", "disable");
+    properties.setProperty("ApplicationName", "ws-collaboration");
+    config.setDataSourceProperties(properties);
+
     return new HikariDataSource(config);
   }
 
